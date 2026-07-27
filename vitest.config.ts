69b67f5a3c035851a3ca23facc87b366dfc82c8a
Takehiro Kitajima <87166470+tkhrktjm@users.noreply.github.com>
2026-07-27 21:00:06 +0900

    Enable instrumented code coverage (istanbul)
    ```
    pnpm add --save-dev @vitest/coverage-istanbul
    ```

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig, mergeConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config.ts";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: "istanbul", // or 'v8'
+        exclude: [...coverageConfigDefaults.exclude],
+      },
       globals: true,
       environment: "happy-dom",
       setupFiles: ["./vitest.setup.ts"],
